184ed8613463dc2a4a72353eaeadddd251c52dca
Damian Birchler <damian.birchler@ost.ch>
2025-05-22 10:05:05 +0200

    WIP

--- scripts/generate-client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-ADDITIONAL_PROPERTIES="packageName=generated_client,library=asyncio"
-rm -rf generated_client && npx openapi-generator-cli generate -i openapi-schema/internal-server-openapi.json -o generated_client -g python --additional-properties=$ADDITIONAL_PROPERTIES
