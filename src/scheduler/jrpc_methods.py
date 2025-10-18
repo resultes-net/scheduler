8f21360bf2f1cf70878b2a0b669dca0d4d5e2dc5
Damian Birchler <damian.birchler@ost.ch>
2025-10-18 16:44:46 +0200

    Improve log forwarding from runner.

--- src/scheduler/jrpc_methods.py
+++ src/scheduler/jrpc_methods.py
@@ -20,5 +20,14 @@ class Context(_rjjs.ContextBase):
 @_jrpcs.method()
 def post_log_message(context: Context, level: int, message: str) -> _jrpcs.Result:
     extra = {"remote_ip": context.ip_address}
-    _LOGGER.log(level, "%s", message, extra=extra)
+
+    # Make sure we see any message coming in from the runner (which messages the runner sends
+    # can be configured on the runner), i.e. ensure that we never log below our root logger's
+    # level. However, if the message coming in is at a higher level, we want to log at that level,
+    # to better convey the urgency.
+    root_logger = _log.getLogger()
+    root_logger_level = root_logger.getEffectiveLevel()
+    scheduler_level = max(level, root_logger_level)
+
+    _LOGGER.log(scheduler_level, "%s", message, extra=extra)
     return _jrpcs.Success()
