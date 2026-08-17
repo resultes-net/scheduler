05f460d3c68f24f0d48946f87ee220e1380c9713
Damian Birchler <damian.birchler@ost.ch>
2026-08-17 16:23:14 +0200

    Correct name of clouds YAML file.

--- src/scheduler/clouds_yaml.py
+++ src/scheduler/clouds_yaml.py
@@ -1,5 +1,5 @@
 import pathlib as _pl
 
 clouds_yaml_file_path = (
-    _pl.Path(__file__).parents[2] / "config" / "secrets" / "PCU-PPGL7VO-clouds.yaml"
+    _pl.Path(__file__).parents[2] / "config" / "secrets" / "openstack-clouds.yaml"
 )
